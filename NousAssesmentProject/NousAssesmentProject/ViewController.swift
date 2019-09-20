01730ecaba116f9734132cdafd2475b1398f2e31
Atalay <atalayasa94@gmail.com>
2019-09-20 15:33:56 +0300

    Project created and gitignore added

---
+++ NousAssesmentProject/NousAssesmentProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NousAssesmentProject
+//
+//  Created by Atalay Asa on 20.09.2019.
+//  Copyright © 2019 Atalay Asa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
